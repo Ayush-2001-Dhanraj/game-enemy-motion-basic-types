d9c7d5c1caf01dc3bc2a5dd7270311ffc06b2a16
Ayush4NattyHatty <dhanrajaayush123@gmail.com>
2023-10-21 22:54:51 +0530

    enemy 3 motion

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const canvas1 = document.getElementById("canvas1");
 const canvas1ctx = canvas1.getContext("2d");
 const CANVAS1_HEIGHT = (canvas1.height = 700);
 const CANVAS1_WIDTH = (canvas1.width = 500);
-const numberOfEnemies = 20;
 
 // endless horizonatl movement
 const canvas2 = document.getElementById("canvas2");
@@ -12,14 +11,24 @@ const canvas2ctx = canvas2.getContext("2d");
 const CANVAS2_HEIGHT = (canvas2.height = 700);
 const CANVAS2_WIDTH = (canvas2.width = 500);
 
+// different pattern movement animation
+const canvas3 = document.getElementById("canvas3");
+const canvas3ctx = canvas3.getContext("2d");
+const CANVAS3_HEIGHT = (canvas3.height = 700);
+const CANVAS3_WIDTH = (canvas3.width = 500);
+
+let gameFrame = 0;
+const numberOfEnemies = 100;
 const type1EnemyArr = [];
 const type2EnemyArr = [];
+const type3EnemyArr = [];
 
 const enemyImage1 = new Image();
 enemyImage1.src = "enemy1.png";
 const enemyImage2 = new Image();
 enemyImage2.src = "enemy2.png";
-let gameFrame = 0;
+const enemyImage3 = new Image();
+enemyImage3.src = "enemy3.png";
 
 class EnemyType1 {
   constructor() {
@@ -89,19 +98,64 @@ class EnemyType2 {
   }
 }
 
+class EnemyType3 {
+  constructor() {
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas3.width - this.width);
+    this.y = Math.random() * (canvas3.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 2 + 0.5;
+  }
+  update() {
+    this.x =
+      (canvas3.width / 2) * Math.sin(this.angle * (Math.PI / 200)) +
+      (canvas3.width / 2 - this.width / 2);
+    this.y =
+      (canvas3.height / 2) * Math.cos(this.angle * (Math.PI / 250)) +
+      (canvas3.height / 2 - this.height / 2);
+    this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = canvas3.width;
+    this.frame = Math.floor(gameFrame / this.flapSpeed) % 6;
+  }
+  draw() {
+    canvas3ctx.drawImage(
+      enemyImage3,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
 for (let i = 0; i < numberOfEnemies; i++) {
   type1EnemyArr.push(new EnemyType1());
   type2EnemyArr.push(new EnemyType2());
+  type3EnemyArr.push(new EnemyType3());
 }
 
 function animate() {
   canvas1ctx.clearRect(0, 0, CANVAS1_WIDTH, CANVAS1_HEIGHT);
   canvas2ctx.clearRect(0, 0, CANVAS2_WIDTH, CANVAS2_HEIGHT);
+  canvas3ctx.clearRect(0, 0, CANVAS3_WIDTH, CANVAS3_HEIGHT);
   type1EnemyArr.forEach((enemy) => {
     enemy.update();
     enemy.draw();
   });
   type2EnemyArr.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  type3EnemyArr.forEach((enemy) => {
     enemy.update();
     enemy.draw();
   });
